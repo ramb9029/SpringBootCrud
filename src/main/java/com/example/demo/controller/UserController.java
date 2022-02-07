3565384b1d7e36a712891e74d6b2e299dbabf975
Zakhar Gubernatorov <ramb9029@gmail.com>
2022-02-08 00:33:41 +0700

    Sbring boot with repository

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,60 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model) {
+        List<User> users = userService.getAllUsers();
+        model.addAttribute("users", users);
+        return "users-list";
+    }
+
+    @GetMapping("/user-create")
+    public String createUserForm(User user) {
+        return "user-create";
+    }
+
+    @PostMapping("/user-create")
+    public String createUser(User user) {
+        userService.saveUser(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("user-delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id){
+        userService.deleteById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/user-update/{id}")
+    public String updateUserForm(@PathVariable("id") Long id, Model model){
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
+        return "user-update";
+    }
+
+    @PostMapping("/user-update")
+    public String updateUser(User user){
+        userService.saveUser(user);
+        return "redirect:/";
+    }
+}
